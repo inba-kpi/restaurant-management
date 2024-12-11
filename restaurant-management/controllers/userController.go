ad9cc1b43f229557573cf4a2968733dee5d75d9d
inba <inba.kpi@gmail.com>
2024-12-11 05:40:33 +0530

    Add files via upload

---
+++ restaurant-management/controllers/userController.go
@@ -0,0 +1,31 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc  {
+	return func (c *gin.Context){
+		
+	}
+	
+}
+
+func GetUser() gin.HandlerFunc{
+	return func(c*gin.Context){
+
+	}
+	
+}
+
+func SigUp() gin.HandlerFunc{
+	return func(c *gin.Context){
+
+	}
+}
+
+func HashPassword(password string) string{
+
+}
+
+func VerifyPassword(userPassword string, providePassword string) (bool, string) {
+	
+}
